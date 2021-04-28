cc02bad401c23fb17b8453a70c3159edc0359aee
Andrzej <andrzej.ganda@gmail.com>
2021-04-28 17:12:33 +0200

    Welcome class add
    Class contains welcome message and provides user with list of available options

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import errno
-
-try:
-    with open("todoList.txt", "a") as f:
-        f.write(input("Add to list: ")+"\n")
-    f.close()
-except Exception as exc:
-    if exc.errno == errno.ENOENT:
-        print("The file doesn't exist.")
-    elif exc.errno == errno.EMFILE:
-        print("You've opened too many files.")
-    else:
-        print("The error number is:", exc.errno)
+class Todo:
+    def addNote(self):
+        try:
+            with open("todoList.txt", "a") as f:
+                f.write(input("Add to list: ")+"\n")
+            f.close()
+        except Exception as exc:
+            if exc.errno == errno.ENOENT:
+                print("The file doesn't exist.")
+            elif exc.errno == errno.EMFILE:
+                print("You've opened too many files.")
+            else:
+                print("The error number is:", exc.errno)
+class Welcome():
+    def hello(self):
+        print("Welcome to the todo list\n"
+        +"Show all notes: select 1\n"
+        +"Add new note: select 2\n"
+        +"Delete note with particural number: select 3\n"
+        +"Draw a note: select 4\n")
+    
+obj = Welcome()
+obj.hello()
+obj2 = Todo()
+obj2.addNote()
